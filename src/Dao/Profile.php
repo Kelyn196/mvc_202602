91e9185f4286ba79529913dc70e29846e538b3e8
Kelyn196 <ariasleonela360@gmail.com>
2026-06-22 21:33:10 -0600

    primeras actualizaciones

---
+++ src/Dao/Profile.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Dao;
+
+class Profile {
+
+    public static function ObtenerProfile(){
+        // Arreglo de Arreglos
+        // Lista de Diccionarios
+        return [
+            "codigo_curso" => "IF352",
+            "desc_curso" => "Negocios Web",
+            "seccion_curso" => "2001",
+            "docente_curso" => "Orlando Betancourth",
+            "docente_codigo" => "6836",
+            "bio_curso" => "Manejo de Comercios Electronicos utilizando PHP y MVC",
+            "matriculados" => 50
+        ];
+    }
+
+    public static function ObtenerPerfiles ()
+    {
+        return [
+            [
+                "codigo_curso" => "IF352",
+                "desc_curso" => "Negocios Web",
+                "seccion_curso" => "2001",
+                "docente_curso" => "Orlando Betancourth",
+                "docente_codigo" => "6836",
+                "bio_curso" => "Manejo de Comercios Electronicos ut",
+                "matriculados" => 50
+            ],
+            [
+                "codigo_curso" => "IF351",
+                "desc_curso" => "Portales Web II",
+                "seccion_curso" => "1901",
+                "docente_curso" => "Orlando Betancourth",
+                "docente_codigo" => "6836",
+                "bio_curso" => "Portales Web Recargado",
+                "matriculados" => 60
+            ],
+            [
+                "codigo_curso" => "IF350",
+                "desc_curso" => "Portales Web I",
+                "seccion_curso" => "1801",
+                "docente_curso" => "Orlando Betancourth",
+                "docente_codigo" => "6836",
+                "bio_curso" => "Portales Web Inicios",
+                "matriculados" => 25
+            ]
+        ];
+        
+    }
+}
